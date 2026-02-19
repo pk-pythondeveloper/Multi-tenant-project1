23b6277bdbc6a9a4256cf6b355dee240dd4841ca
Nish898 <shuklanisha898@gmail.com>
2026-02-19 15:09:15 +0530

    this code is change by prince branch

---
+++ task/home/new_lakhan.py
@@ -0,0 +1,14 @@
+a="this variable is declared by lakhan branch"
+
+
+def fun_for_validate(t):
+    print(t)
+
+fun_for_validate("this function made by lakhan branch ")
+
+
+
+def this_function_solve_merge_conflict(t):
+    print(t)
+
+this_function_solve_merge_conflict("this function made by lakhan branch (this line added bnakul branch) (this change is don by prince branch")  )
