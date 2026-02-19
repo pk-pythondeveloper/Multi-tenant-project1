a9a0358e95b0b64b27ad968c51d598f868078a96
Nish898 <shuklanisha898@gmail.com>
2026-02-19 15:41:38 +0530

    here i change the commit name

--- task/home/new_lakhan.py
+++ task/home/new_lakhan.py
@@ -12,3 +12,7 @@ def this_function_solve_merge_conflict(t):
     print(t)
 
 this_function_solve_merge_conflict("this function made by lakhan branch (this line added bnakul branch) (this change is don by prince branch")  )
+
+
+
+print("here we try to change the commit anme")
